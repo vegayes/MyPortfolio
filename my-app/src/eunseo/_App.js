402cb7519a136a7e83913d644343ead07b5b666b
vegayes <vegayes@naver.com>
2024-02-07 20:42:28 +0900

    eunseo

---
+++ my-app/src/eunseo/_App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App(){
+    return (
+        <div>
+            <h1>start react 200!</h1>
+            <p> Html 적용하기</p>
+        </div>
+    )
+}
+
+export default App;
