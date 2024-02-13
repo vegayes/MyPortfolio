fee7c5234f9f45e2a0d534bc7581afce112f8436
vegayes <vegayes@naver.com>
2024-02-13 22:59:08 +0900

    eunseo

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -29,3 +29,76 @@ function App() {
 }
 
 export default App;
+
+
+/*
+  JSX 문법
+*/
+
+// 1) 닫힌 태그 사용
+
+// 2) 반드시 부모 요소 하나가 감싸는 형태여야 함.
+// div , fragment , <></>
+
+/*
+function App() {
+	return (
+		<>
+			<div>Hello</div>
+			<div>Eunseo!</div>
+		</>
+	);
+}*/
+
+
+// 3) 자바스크립트 표현식
+// 내부 코드에 {} 감싸줌
+
+/*
+function App() {
+	const name = 'Eunseo';
+	return (
+		<div>
+			<div>Hello</div>
+			<div>{name}!</div>
+		</div>
+	);
+}
+*/
+
+// 4) 삼항 연산자 사용
+// 4-1) 외부 
+/*
+function App() {
+	let desc = '';
+	const loginYn = 'Y';
+	if(loginYn === 'Y') {
+		desc = <div>Eusneo 입니다.</div>;
+	} else {
+		desc = <div>비회원 입니다.</div>;
+	}
+	return (
+		<>
+			{desc}
+		</>
+	);
+} 
+*/
+
+// 4-2) 내부
+/* 
+function App() {
+	const loginYn = 'Y';
+	return (
+		<>
+			<div>
+				{loginYn === 'Y' ? (
+					<div>Eunseo 입니다.</div>
+				) : (
+					<div>비회원 입니다.</div>
+				)}
+			</div>
+		</>
+	);
+}
+*/
