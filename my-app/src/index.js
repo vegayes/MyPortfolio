6a6fc4c1ed81dd82f499b2e7f66e3844516c7f3e
vegayes <vegayes@naver.com>
2024-02-14 21:17:44 +0900

    eunseo

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,12 +4,24 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+import Clock from "./eunseo/chapter04/clock"
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+setInterval(()=> {
+  ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+    <Clock />
+  </React.StrictMode>,
+  document.getElementById('root')
+  );
+},1000);
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
